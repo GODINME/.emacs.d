96c12f63196d7870345afdb8b4f8aa9e0b87ea37
GODINME <poffei@st.ug.edu.gh>
2022-05-10 10:20:35 +0100

    added setup for easy use of coq in emacs

--- init.el
+++ init.el
@@ -1,19 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(proof-general which-key try use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
- ;; Automatic generated info 
-
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -64,7 +48,7 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inhibit-strtup-message t)
+(setq inhibit-startup-message t)
 (setq auto-save-default nil)
 (setq delete-auto-save-files t)
 
